7c6d34fa007535fa983b47d96b926ca3f6160d35
xxjpbarx <jaypeebarrientos9@gmail.com>
2024-10-29 16:07:21 +0800

    menu.php

--- menu.php
+++ menu.php
@@ -137,38 +137,8 @@
 <body>
 
     <!--=============== NAVIGATION BAR ===============-->
-    <nav class="navbar">
-        <div class="navbar-container">
-            <div class="navbar-brand">
-                <a href="main.php">
-                    <img src="images/Logo.png" alt="Logo"/>
-                </a>
-            </div>
-
-            <ul class="navbar-nav-left">
-                <li><a href="menu.php">Menu</a></li>
-                <li><a href="listoforder.php">Order Now</a></li>
-                <li><a href="contact.php">Contact</a></li>
-            </ul>
-
-            <ul class="navbar-nav-right">
-                <li>
-                    <a href="stores.php">
-                        <img src="images/marker.svg" alt="" />
-                        <span>Find a store</span>
-                    </a>
-                </li>
-                <a href="login.php">
-                    <button class="btn btn-dark-outline">Sign in</button>
-                </a>
-                <li>
-                    <a href="signup.php">
-                        <button class="btn btn-dark">Sign Up</button>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+  
+    <?php include 'navigation.php' ?>
 
     <!--=============== HAMBURGER MENU ===============-->
     <button type="button" class="hamburger" id="menu-btn">
